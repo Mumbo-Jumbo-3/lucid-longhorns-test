1c48ca2bf54d63a626165a7dafe60e407d004ad0
HairyBarry <108812726+Mumbo-Jumbo-3@users.noreply.github.com>
2022-07-13 00:01:44 -0400

    Update index.tsx

--- packages/nouns-webapp/src/index.tsx
+++ packages/nouns-webapp/src/index.tsx
@@ -82,6 +82,8 @@ export type AppDispatch = typeof store.dispatch;
 const supportedChainURLs = {
   [ChainId.Mainnet]: createNetworkHttpUrl('mainnet'),
   [ChainId.Rinkeby]: createNetworkHttpUrl('rinkeby'),
+  [ChainId.ArbitrumRinkeby]: createNetworkHttpUrl('arbTest'),
+  [ChainId.Arbitrum]: createNetworkHttpUrl('arbitrum'),
   [ChainId.Hardhat]: 'http://localhost:8545',
 };
 
