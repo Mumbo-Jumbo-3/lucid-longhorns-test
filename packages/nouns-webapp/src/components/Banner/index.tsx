5f6f272eaceff22d21e55cf648ffa9c10814d788
HairyBarry <108812726+Mumbo-Jumbo-3@users.noreply.github.com>
2022-07-13 01:21:50 -0400

    Update index.tsx

--- packages/nouns-webapp/src/components/Banner/index.tsx
+++ packages/nouns-webapp/src/components/Banner/index.tsx
@@ -1,8 +1,7 @@
 import classes from './Banner.module.css';
 import Section from '../../layout/Section';
 import { Col } from 'react-bootstrap';
-import calendar_noun from '../../assets/calendar_noun.png';
-import Noun from '../Noun';
+import cows from '../../assets/cows.jpg';
 import { Trans } from '@lingui/macro';
 
 const Banner = () => {
@@ -11,17 +10,17 @@ const Banner = () => {
       <Col lg={6}>
         <div className={classes.wrapper}>
           <h1>
-            <Trans>ONE LUCID LONGHORN,</Trans>
+            <Trans>ONE LUCID LONGHORN.</Trans>
             <br />
-            <Trans>EVERY 8 HOURS,</Trans>
+            <Trans>EVERY 8 HOURS.</Trans>
             <br />
             <Trans>FOREVER.</Trans>
           </h1>
         </div>
       </Col>
       <Col lg={6}>
-        <div style={{ padding: '2rem' }}>
-          <Noun imgPath={calendar_noun} alt="noun" />
+        <div className={classes.centerImage}>
+          <img src={cows} alt="longhorns"/>
         </div>
       </Col>
     </Section>
